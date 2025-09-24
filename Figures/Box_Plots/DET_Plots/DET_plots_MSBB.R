f2d904aeedb7c9da16dbeccefe00ba5b360b70d8
RumeysaAksu <102908542+RumeysaAksu@users.noreply.github.com>
2025-09-25 00:38:26 +0300

    Update DET_plots_MSBB.R

--- Figures/Box_Plots/DET_Plots/DET_plots_MSBB.R
+++ Figures/Box_Plots/DET_Plots/DET_plots_MSBB.R
@@ -91,6 +91,83 @@ ggsave(filename = file.path(path, "GNG4_Expressions_AD_vs_AsymAD_MSBB_DET.tiff")
 
 
 
+# Gene: MRPL1 ENSG00000169288
+
+
+transcripts_of_interest <- c("ENST00000515625.1", "ENST00000511521.1", "ENST00000506674.1", "ENST00000504901.2", "ENST00000502384.3", "ENST00000315567.13")
+
+
+x <- rownames(fitteddata)
+
+
+transcripts_of_interest %in% x
+
+
+fitteddata_with_rownames <- data.frame(RowNames = row.names(fitteddata), fitteddata, check.names = FALSE)
+
+
+
+# AD vs AsymAD countdata
+
+count_AD_Asym <- fitteddata_with_rownames[, c(1, 22:182)]
+
+col_Data_3 <- col_Data[ c(21:181), ]
+
+
+
+# Filter data
+
+filtered_data_1 <- count_AD_Asym[count_AD_Asym$RowNames %in% transcripts_of_interest, ]
+
+
+library(tidyverse)
+
+# Reshape the data to long format for ggplot2
+long_data_1 <- filtered_data_1 %>%
+  pivot_longer(cols = -RowNames, names_to = "Sample", values_to = "Expression")
+
+# Add group information from col_Data_3
+col_Data_3 <- col_Data_3 %>%
+  mutate(Sample = rownames(col_Data_3))
+
+long_data_1 <- long_data_1 %>%
+  left_join(col_Data_3, by = c("Sample"))
+
+
+
+# Create the plot
+ggplot(long_data_1, aes(x = group, y = Expression, color = group, shape = group, fill = group)) +
+  geom_boxplot(alpha = 0.3, outlier.shape = NA) +
+  geom_jitter(size = 2, alpha = 0.7, position = position_jitter(width = 0.2)) +
+  theme_minimal() +
+  labs(title = "Transcripts of MRPL1 Expressions in AD vs AsymAD (MSBB)",
+       x = "Group", y = "Expression") +
+  scale_color_manual(values = c("AD" = "skyblue", "AsymAD" = "darkblue")) +
+  scale_fill_manual(values = c("AD" = "skyblue", "AsymAD" = "darkblue")) +
+  geom_signif(comparisons = list(c("AD", "AsymAD")), 
+              map_signif_level = TRUE, textsize = 3, test = "t.test", color = "black") +
+  facet_wrap(~ RowNames, scales = "free") +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +  # Ensure 4 breaks on y-axis
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.text.y = element_text(size = 4), # Adjust Y-axis tick label size
+    legend.title = element_text(size = 10),
+    legend.text = element_text(size = 8),
+    panel.grid.major = element_blank(), # Remove large grids
+    panel.grid.minor = element_blank(),  # Remove small grids
+    axis.line = element_line(color = "black")  # Add black axis lines
+  )
+
+
+path <- "H:/plots_last/DET_plots"
+
+ggsave(filename = file.path(path, "MRPL1_Expressions_AD_vs_AsymAD_MSBB_DET.tiff"),
+       plot = last_plot(),
+       width = 9, height = 7, device = "tiff", dpi = 600)
+
+
+
 
 # Gene: ADAMTS2 ENSG00000087116
 
@@ -173,83 +250,6 @@ ggsave(filename = file.path(path, "ADAMTS2_Expressions_AD_vs_AsymAD_MSBB_DET.tif
        plot = last_plot(),
        width = 9, height = 7, device = "tiff", dpi = 600)
 
-
-
-
-# Gene: MRPL1 ENSG00000169288
-
-
-transcripts_of_interest <- c("ENST00000515625.1", "ENST00000511521.1", "ENST00000506674.1", "ENST00000504901.2", "ENST00000502384.3", "ENST00000315567.13")
-
-
-x <- rownames(fitteddata)
-
-
-transcripts_of_interest %in% x
-
-
-fitteddata_with_rownames <- data.frame(RowNames = row.names(fitteddata), fitteddata, check.names = FALSE)
-
-
-
-# AD vs AsymAD countdata
-
-count_AD_Asym <- fitteddata_with_rownames[, c(1, 22:182)]
-
-col_Data_3 <- col_Data[ c(21:181), ]
-
-
-
-# Filter data
-
-filtered_data_1 <- count_AD_Asym[count_AD_Asym$RowNames %in% transcripts_of_interest, ]
-
-
-library(tidyverse)
-
-# Reshape the data to long format for ggplot2
-long_data_1 <- filtered_data_1 %>%
-  pivot_longer(cols = -RowNames, names_to = "Sample", values_to = "Expression")
-
-# Add group information from col_Data_3
-col_Data_3 <- col_Data_3 %>%
-  mutate(Sample = rownames(col_Data_3))
-
-long_data_1 <- long_data_1 %>%
-  left_join(col_Data_3, by = c("Sample"))
-
-
-
-# Create the plot
-ggplot(long_data_1, aes(x = group, y = Expression, color = group, shape = group, fill = group)) +
-  geom_boxplot(alpha = 0.3, outlier.shape = NA) +
-  geom_jitter(size = 2, alpha = 0.7, position = position_jitter(width = 0.2)) +
-  theme_minimal() +
-  labs(title = "Transcripts of MRPL1 Expressions in AD vs AsymAD (MSBB)",
-       x = "Group", y = "Expression") +
-  scale_color_manual(values = c("AD" = "skyblue", "AsymAD" = "darkblue")) +
-  scale_fill_manual(values = c("AD" = "skyblue", "AsymAD" = "darkblue")) +
-  geom_signif(comparisons = list(c("AD", "AsymAD")), 
-              map_signif_level = TRUE, textsize = 3, test = "t.test", color = "black") +
-  facet_wrap(~ RowNames, scales = "free") +
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +  # Ensure 4 breaks on y-axis
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    axis.text.y = element_text(size = 4), # Adjust Y-axis tick label size
-    legend.title = element_text(size = 10),
-    legend.text = element_text(size = 8),
-    panel.grid.major = element_blank(), # Remove large grids
-    panel.grid.minor = element_blank(),  # Remove small grids
-    axis.line = element_line(color = "black")  # Add black axis lines
-  )
-
-
-path <- "H:/plots_last/DET_plots"
-
-ggsave(filename = file.path(path, "MRPL1_Expressions_AD_vs_AsymAD_MSBB_DET.tiff"),
-       plot = last_plot(),
-       width = 9, height = 7, device = "tiff", dpi = 600)
 
 
 
