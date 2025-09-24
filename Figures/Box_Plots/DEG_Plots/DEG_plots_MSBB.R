4b48bbe6ed0b97a2e1f13ef070ab7d14c47363ad
RumeysaAksu <102908542+RumeysaAksu@users.noreply.github.com>
2025-09-24 22:39:33 +0300

    Update DEG_plots_MSBB.R

--- Figures/Box_Plots/DEG_Plots/DEG_plots_MSBB.R
+++ Figures/Box_Plots/DEG_Plots/DEG_plots_MSBB.R
@@ -162,31 +162,31 @@ col_Data_3 <- col_Data[ c(21:181), ]
 
 
 # Filter data
-filtered_data_1 <- count_AD_Asym[rownames(count_AD_Asym) %in% gene_of_interest, ]
+filtered_data_2 <- count_AD_Asym[rownames(count_AD_Asym) %in% gene_of_interest, ]
 
-filtered_data_1 <- as.data.frame(filtered_data_1)
-
-
+filtered_data_2 <- as.data.frame(filtered_data_2)
 
 
-filtered_data_1 <- filtered_data_1 %>%
+
+
+filtered_data_2 <- filtered_data_2 %>%
   rownames_to_column(var = "Sample")  
 
-filtered_data_1 <- as.data.frame(filtered_data_1)
+filtered_data_2 <- as.data.frame(filtered_data_2)
 
 
 
-long_data_1 <- filtered_data_1 %>%
+long_data_2 <- filtered_data_2 %>%
   pivot_longer(cols = -Sample, names_to = "sample_id", values_to = "Expression")
 
 # Join long_data_2 with col_Data_3 to get the group information
-long_data_1 <- long_data_1 %>%
+long_data_2 <- long_data_2 %>%
   left_join(col_Data_3, by = "sample_id")
 
 
 
 # Create the plot
-ggplot(long_data_1, aes(x = group, y = Expression, color = group, fill = group, shape = group)) +
+ggplot(long_data_2, aes(x = group, y = Expression, color = group, fill = group, shape = group)) +
   geom_boxplot(alpha = 0.3, outlier.shape = NA, position = position_dodge(width = 0.75)) +
   geom_jitter(size = 2, alpha = 0.7, position = position_jitter(width = 0.2, height = 0)) +
   theme_minimal() +
